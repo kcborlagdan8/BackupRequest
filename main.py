eea5f442a524c2da1689cb7ac501eba514abadff
KC Borlagdan <kcborlagdan8@github.io>
2021-12-08 17:54:00 +0800

    deleted project.py and added data consent functionality

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
+from dns.rdatatype import NULL
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 import re
 from sftp import SFTP
 import lookup
+import sys
 
-wb = load_workbook('.\BackupRequests.xlsx')
+file = sys.argv[1]
+
+search = "database backup request" if "backuprequest" in str(file).lower() else "support server (not support pod)"
+wb = load_workbook(file)
 ws = wb.active
+count = 0 #number of tickets
+dbtype = 'P'
+dbserver = ''
 
-count = 0#number of tickets
+def dbtypealias(dbtype):
+    switcher = {
+        "Preview": "D",
+        "Production": "P",
+        "Sandbox": "S"
+    }
+    return switcher.get(dbtype, "XXXXXX")
 
 #loop through rows for backup requests
 for row in range(1, 300): #row
     for col in range(1, 7): #column
         char = get_column_letter(col)
         val = ws[char + str(row)].value
-        if(col == 6 and val != None and "Database Backup Request" in str(val)):
+        if(col == 6 and val is not None and search in str(val).lower()):
             count += 1
+            
             #get values from excel
             clientID = ws["D" + str(row)].value
             client = ws["E" + str(row)].value
@@ -23,51 +38,45 @@ for row in range(1, 300): #row
             product = ws["C" + str(row)].value
 
             #Generate sftp credentials
-            session = SFTP().credGen(case)
-            credentials = SFTP().getCred(session)
+            try:
+                session = SFTP().credGen(case)
+                credentials = SFTP().getCred(session)
+            except Exception:
+                credentials = ""
 
             #Get instance name 
             domain = ws["G" + str(row)].value #get dlz value
             try:
-                instance = re.search('https://(.+?).dlz.deltek.com', domain).group(1).title()
+                instance = re.search('(.+?)@(.+?).com', domain).group(2).title()
             except AttributeError:
                 instance = ""
             #end get instance name
 
-            #get db name - not accurate for VT23
-
-
             #Get database type
             subject = ws["F" + str(row)].value #check if preview, sandbox, production
-            dbtype = subject.split("-")
-            dbtype = dbtype[1].strip()
-            def dbtypealias(dbtype):
-                switcher = {
-                    "Preview": "D",
-                    "Production": "P",
-                    "Sandbox": "S"
-                }
-                return switcher.get(dbtype, "XXXXXX")
-            #end database type
-
-            dbtype = dbtypealias(dbtype)
+            if(search == "database backup request"):
+                dbtype = subject.split("-")
+                dbtype = dbtype[1].strip()
+                dbtype = dbtypealias(dbtype)
+            #end database type           
 
             
             #get DB Server if NOT PREVIEW
             if(dbtype != "D"): 
                 try:   
-                    fqdn = lookup.dnsresolver(instance + ".deltekfirst.com") 
+                    fqdn = lookup.dnsresolver(f"{instance}.deltekfirst.com") 
                 except:
-                    print("\nFQDN not found for " + instance + ", " + clientID + ". Check if the client has different instance name.")
+                    print(f"\nFQDN not found for {instance}, {clientID}. Check if the client has different instance name.")
                     fqdn = ""
 
                 if(product == "Vision"):
-                    dbserver = lookup.getDBfromCNAME(str(fqdn))
-                try:
-                    pod = re.search('vt(.+?).deltekfirst.com.', fqdn).group(1)
-                    dbserver = "USEAPDVT" + pod +"DB1"
-                except AttributeError:
-                    pod = ""
+                    dbserver = lookup.getDBfromCNAME(fqdn)
+                else:
+                    try:
+                        pod = re.search('vt(.+?).deltekfirst.com.', fqdn).group(1)
+                        dbserver = f"USEAPDVT{pod}DB1"
+                    except AttributeError:
+                        pod = ""
             else:
                 dbserver = "USEAPVVT0DB1"
 
@@ -82,13 +91,11 @@ for row in range(1, 300): #row
                     + "_1_" \
                     + ((instance.upper() + "00000000000")[:11]) #e.g.C000073333P_1_ontoit00000
 
-            
             if(dbtype == "S" and product == "Vision"):
                 databaseName += "_Sandbox"
     
             #print(databaseName)
-            print( dbserver + "," + databaseName + "," + clientID + "," + "N,Y," + "\"" + client + "\"," + case + "," + credentials
-            )
+            print(f"{dbserver},{databaseName},{clientID},N,Y,\"{client}\",{case},{credentials}")
 
             
-print("\nNumber of Backup Requests: " + str(count))
+print(f"\nNumber of Backup Requests: {str(count)}")
--- project.py
+++
@@ -1,7 +0,0 @@
-from sftp import SFTP
-
-list = ['211118-000760']
-for l in list:
-    html = SFTP().credGen(l)
-    credentials = SFTP().getCred(html)
-    print(credentials)
