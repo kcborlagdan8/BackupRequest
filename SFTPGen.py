d5e159aa15992a9ea0ba94b41ccf3d57b5af3ddf
KC Borlagdan <kcborlagdan8@github.io>
2021-12-22 16:55:26 +0800

    Changed sftpgen.py to get argument instead of hardcoding RNT

--- SFTPGen.py
+++ SFTPGen.py
@@ -1,7 +1,12 @@
 from sftp import SFTP
+import sys
 
-list = ['211013-000245']
-for l in list:
-    html = SFTP().credGen(l)
-    credentials = SFTP().getCred(html)
-    print(credentials)
+def main():
+    list = sys.argv[1:]
+    for l in list:
+        html = SFTP().credGen(l)
+        credentials = SFTP().getCred(html)
+        print(credentials)
+
+if __name__ == '__main__':
+    main()
